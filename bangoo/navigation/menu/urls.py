208adebbb0473dd0f03da8fe1f8800861ca30751
Schon Peter <slapec@codehouse.hu>
2014-12-17 09:39:48 +0100

    Menu app is under Navigation app now

--- bangoo/navigation/menu/urls.py
+++ bangoo/navigation/menu/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from bangoo.content.menu import views
+from bangoo.navigation.menu import views
 
 urlpatterns = patterns('',
     url(r'^$', views.menu, name='admin-menu'),
