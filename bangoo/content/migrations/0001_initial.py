d7cb625a26bf83433ca938473d686889a7897d80
Schon Peter <slapec@codehouse.hu>
2015-02-05 15:40:13 +0100

    Author does not subclasses User anymore

--- bangoo/content/migrations/0001_initial.py
+++ bangoo/content/migrations/0001_initial.py
@@ -15,15 +15,13 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Author',
             fields=[
-                ('user_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('experience', models.CharField(max_length=10, choices=[(b'begin', b'Beginner'), (b'inter', b'Itermediate'), (b'expert', b'Expert')])),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
-                'abstract': False,
-                'verbose_name': 'user',
-                'verbose_name_plural': 'users',
             },
-            bases=('auth.user',),
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Content',
