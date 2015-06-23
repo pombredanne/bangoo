dae3913fd681f47330cc4e663891b3e12338562a
Schon Peter <slapec@codehouse.hu>
2015-06-23 11:06:21 +0200

    Permission and js fixes
    FIX: Including common.js was missing from admin page
    FIX: Login required to access admin index page

--- bangoo/admin/views/base.py
+++ bangoo/admin/views/base.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from bangoo.navigation.utils import get_urlconf
 from django.core.urlresolvers import resolve
 
 
+@login_required
 def home(request, template_name='admin/home.html'):
     return render(request, template_name)
 
 
+@login_required
 def admin_menu_dispatcher(request, menu_id):
     uconf = get_urlconf(request.act_menu.plugin, frontend_urls=False)
 
